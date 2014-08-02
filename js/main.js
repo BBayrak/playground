22bb394266b8aaaccd4e9a3dc3b7992e673cd472
Bahtiyar Bayrak <bahtiyar.bayrak@gmx.de>
2014-08-02 18:35:23 +0200

    - jquery-streps integriert

--- js/main.js
+++ js/main.js
@@ -1,25 +1,13 @@
 $( document ).ready(function() {
-	$( "a" ).click(  function( event ) {
-			alert( "The link will no longer take you to jquery.com" );
-			event.preventDefault();	
-	});
+
+	$("#wizard").steps({transitionEffect: "slideLeft"});
+
 	
-	$( "#date" ).datepicker();
-	
-	$( "#slider" ).slider({
-		min:0,
-		max: 50,
-		step:1,
-		value: 50,
-		change: function( event, ui ) {
-			// getter
-			var value = $( "#slider" ).slider( "option", "value" );
-		//	alert( "The link will no longer take you to jquery.com "  + value);
-			$("#quantity").val(value);
-		}
-	});
-	
-	$( "a" ).addClass( "test" );
-	});
+
+
+});
+
+
+
 
 
