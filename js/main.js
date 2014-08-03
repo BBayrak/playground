cc8f24fa8986a4f88ee85d02ae1ca16d9280402a
Bahtiyar Bayrak <bahtiyar.bayrak@gmx.de>
2014-08-03 23:31:00 +0200

    verfeinert

--- js/main.js
+++ js/main.js
@@ -1,43 +1,48 @@
 $( document ).ready(function() {
-	
-	var gender="null";
-	
-	
+
+	var gender="";
+	var nuance="";
+
 
 	$("#wizard").steps({transitionEffect: "slideLeft"});
 
+
 	$('.btn').button()
+
+
+
+	$(".thumbnail").on("mouseenter", function(){
+		$(this).css({"opacity": "0.8","cursor":"pointer"});
+	}).on("mouseleave", function(){
+		$(this).css({"opacity": "1","cursor":"default"});
+	}).on("click", function(){
+		var wp = $(this).closest(".wizardPage");
+		wp.find(".glyphicon").remove();
+		var sign = $("<span class='glyphicon glyphicon-ok-circle glyphicon-2x'></span>");
+		$(this).find(".caption").last().append(sign);
+		$(this).find(".glyphicon").animate({"opacity": "1"}, "fast");
+	});
 
 	$("#genderM").on("click", function(){
 		gender="m";
 		$('#gender').val(gender);
-	})
-	.on("mouseenter",function(){
-		$(this).closest("")
-	})
-	;
+	});
 
 	$("#genderF").on("click", function(){
 		gender="f";
 		$('#gender').val(gender);
-	});
-	
-/*	var bgArray = ['fotolia_35867557.jpg',
-	          	'fotolia_44944823.jpg',
-	        	'fotolia_45779873.jpg',
-	        	'fotolia_50986002.jpg',
-	        	'fotolia_56582519.jpg'];
-	        	*/
+	})	
 
- /*
-	
+
+	/*
+
 	function startTime() {
 	    t = setTimeout(function(){startTime()}, 1000);
 		$("body").removeClass("bg1 bg2 bg3 bg4 bg5 bg6");
 		$("body").addClass("bg"+Math.floor(Math.random() * 2) );
 	}
 
-	
+
 	startTime();*/
 });
 
