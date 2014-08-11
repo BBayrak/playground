a0178ffc8b43156d7f35981332cf05580c913f04
Bahtiyar Bayrak <bahtiyar.bayrak@gmx.de>
2014-08-11 13:18:04 +0200

    bilder ausgetauscht

--- js/main.js
+++ js/main.js
@@ -30,16 +30,18 @@ $( document ).ready(function() {
 	});
 
 	$(".next-step2").on("click", function(){
-		$wizard.steps( "next" );
-		$('#nuance').val($(this).attr("id"));
-		hideAll();
-		$('#wizard').find("."+$(this).attr("id")).show();
-				
+		
 		if($(this).attr("id") == "holzig" )
 		{
 			$('#effect').val("würzig");	
-			$wizard.steps( "next" );
+			$wizard.steps( "nextnext" );
 		}
+		else{
+			$wizard.steps( "next" );
+			$('#nuance').val($(this).attr("id"));
+		}		
+		hideAll();
+		$('#wizard').find("."+$(this).attr("id")).show();		
 	});
 	
 	$(".next-step3").on("click", function(){		
@@ -57,6 +59,7 @@ $( document ).ready(function() {
 		$('#zeit').val($(this).attr("id"));			
 		/* HIER DIE LISTE FÜR NÄCHSTE SEITE VORBEREITEN UNF FÜLLEN
 		 * */
+		getList();
 		$wizard.steps( "next" );
 	});
 	
@@ -73,6 +76,28 @@ $( document ).ready(function() {
 		$('#wizard').find(".orientalisch").hide();
 	}
 	
+	
+	function getList(){
+
+
+		$.getJSON( "duftliste.json", function( data ) {
+			var items = [];
+			$.each( data, function( key, val ) {
+
+				var snippet = '<div class="col-xs-4 col-sm-3 placeholder">'+
+				'<img src="img/3772347-Pien-Flasche-2.jpg"'+
+					'class="img-responsive" alt="Generic placeholder thumbnail">'+
+				'<p><button type="button" class="btn btn-default btn-block" >'+ key +'</button></p>'+
+				'</div>';
+				
+				$("#empfehlungen").first().append(snippet);
+				
+			});
+			
+		});
+
+
+	}
 
 	
 	
