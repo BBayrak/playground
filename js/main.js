74efa97c35cd081fac90864010816ec43ab0dbb0
Bahtiyar Bayrak <bahtiyar.bayrak@gmx.de>
2014-08-13 18:11:02 +0200

    owl carousel settings verändert

--- js/main.js
+++ js/main.js
@@ -202,12 +202,16 @@ $( document ).ready(function() {
 	      ajaxDisplay.innerHTML = ajaxRequest.responseText;
 
 	      $(".owl-carousel").owlCarousel({
-	    	  autoPlay: 3000, //Set AutoPlay to 3 seconds
+	    	  /*autoPlay: 3000, //Set AutoPlay to 3 seconds
 	    	  	  
 	    	  items : 4,
 	    	  itemsDesktop : [1199,4],
-	    	  itemsDesktopSmall : [979,5]
-	    	   
+	    	  itemsDesktopSmall : [979,5]*/
+	    	  items : 4, //10 items above 1000px browser width
+	    	  itemsDesktop : [1000,4], //5 items between 1000px and 901px
+	    	  itemsDesktopSmall : [900,3], // betweem 900px and 601px
+	    	  itemsTablet: [600,2], //2 items between 600 and 0
+	    	  itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
 	    	  });
 	      
 
